f95a3c8a209118d23ba1bfe9b4a5a988d1bd60a0
archloy <Archloy@users.noreply.github.com>
2015-11-29 18:51:23 +0100

    add navigation with /pageNumber and /page/pagenumber

--- src/picc/piccBundle/Controller/DefaultController.php
+++ src/picc/piccBundle/Controller/DefaultController.php
@@ -7,15 +7,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     // Action performed by call "index" page
-    public function indexAction()
+    public function indexAction($id)
     {
         // Get the entity repository
         $repo = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
 
-        // Prepare request for last ten 
-        $lasts = $repo->select('p')->from('piccBundle:Pebkac', 'p')->orderBy('p.id', 'DESC')->setMaxResults(10);
+        // Prepare request for last ten and get the query
+        $query = $repo->select('p')->from('piccBundle:Pebkac', 'p')->orderBy('p.id', 'DESC')->setFirstResult(($id-1)*10)->setMaxResults(10)->getQuery();
+
+        $lasts = $query->getResult();
+
+        if($lasts == null)
+            throw $this->createNotFoundException('Page does not exist !');
+
 
         // Call twig index template and associate result of request 
-        return $this->render('piccBundle:Default:index.html.twig', array('piccs' => $lasts->getQuery()->getResult()));
+        return $this->render('piccBundle:Default:index.html.twig', array('piccs' => $lasts));
     }
 }
