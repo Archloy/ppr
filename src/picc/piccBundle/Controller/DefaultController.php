eb72fd0347006198f14007f3bd7436dd0ef0dd65
archloy <Archloy@users.noreply.github.com>
2015-11-30 01:56:14 +0100

    Add Picc overview/comments page

--- src/picc/piccBundle/Controller/DefaultController.php
+++ src/picc/piccBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
     public function indexAction($id)
     {
         // Get the entity repository
-        $repo = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
+        $repo = $this->getDoctrine()->getManager()->createQueryBuilder();
 
         // Prepare request for last ten and get the query
         $query = $repo->select('p')->from('piccBundle:Pebkac', 'p')->orderBy('p.id', 'DESC')->setFirstResult(($id-1)*10)->setMaxResults(10)->getQuery();
@@ -23,5 +23,18 @@ class DefaultController extends Controller
 
         // Call twig index template and associate result of request 
         return $this->render('piccBundle:Default:index.html.twig', array('piccs' => $lasts));
+    }
+
+    // Action performed on pebkac/id and print comments 
+    public function showPiccAction($id)
+    {
+        $picc = $this->getDoctrine()->getRepository('piccBundle:Pebkac')->find($id);
+
+        if(!$picc)
+            throw $this->createNotFoundException('Page does not exist !');
+
+        $comms = $this->getDoctrine()->getRepository('piccBundle:Comments')->findBy(array('wtfId' => $id));
+
+        return $this->render('piccBundle:Default:picc.html.twig', array('picc' => $picc, 'comms' => $comms));
     }
 }
